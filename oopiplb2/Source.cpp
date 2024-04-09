7de9297fdba557c599d68f60f8da79d7fd10b2e4
Inversell <160104168+Inversell@users.noreply.github.com>
2024-04-09 09:59:55 +0300

    Update Source.cpp

--- oopiplb2/Source.cpp
+++ oopiplb2/Source.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 class Contact {
 	string name;
-	string surname;
 	int number;
 
 public:
@@ -13,10 +12,7 @@ public:
 	{
 		this->name = name;
 	}
-	string SetSurname(string surname)
-	{
-		this->surname = surname;
-	}
+
 	int  SetNumber(int number)
 	{
 
@@ -25,39 +21,40 @@ public:
 	string GetName(string name)
 	{
 		return name;
-    }
-	string GetSurname(string surname)
-	{
-		return surname;
-    }
+	}
+	
 	int  GetNumber(int number)
 	{
 		return number;
-    }
-	Contact(string name, string surname, int number)
+	}
+	Contact() 
+	{
+
+	}
+	Contact(string name, int number)
 	{
 		this->name = name;
-		this->surname = surname;
+	
 		this->number = number;
 	}
 	Contact(const Contact& other)
 	{
 		this->name = other.name;
-		this->surname = other.surname;
+	
 		this->number = other.number;
 	}
 	~Contact()
 	{
-	
+
 	}
 	bool operator ==(const Contact& other)
 	{
-		return(this->name == other.name && this->surname == other.surname && this->number == other.number);
+		return(this->name == other.name &&  this->number == other.number);
 	}
 
 	Contact& operator&=(const Contact& other) {
-		if (name == other.name && surname == other.surname && number == other.number) {
-			
+		if (name == other.name &&  number == other.number) {
+
 		}
 		return *this;
 	}
@@ -67,36 +64,37 @@ public:
 	}
 
 	friend istream& operator>>(istream& is, Contact& contact) {
-		std::cout << "Enter name: ";
+		cout << "Enter name: ";
 		is >> contact.name;
-		std::cout << "Enter phone number: ";
+		cout << "Enter phone number: ";
 		is >> contact.number;
 		return is;
 	}
 };
 int main()
 {
-	Contact contact1("HH", "dd", 4743789);
-	Contact contact2("tt","ew", 446834);
+	Contact contact1("Name", 123456);
+	Contact contact2("Name", 123456);
 
 	cout << "Contact 1: " << contact1 << endl;
 	cout << "Contact 2: " << contact2 << endl;
 
 	if (contact1 == contact2) {
-		cout << "Contacts are the same" << endl;
+		cout << "Contacts are the same" << endl;\
+		Contact mergedContact = contact1;
+		cout << "Merged Contact: " << mergedContact << endl;
 	}
 	else {
 		cout << "Contacts are different" << endl;
 	}
 
-	Contact mergedContact = contact1;
-	mergedContact.SetNumber(contact2.GetNumber());
 
-	std::cout << "Merged Contact: " << mergedContact << std::endl;
+
+	
 
 	Contact newContact;
-	std::cin >> newContact;
-	std::cout << "New Contact: " << newContact << std::endl;
+	cin >> newContact;
+	cout << "New Contact: " << newContact << endl;
 
 	return 0;
 }
